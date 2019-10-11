ba11eb4df2ddd2e1a6a625a104ea99e6eb3d0314
Vicenzo <v.a.s.i@outlook.com>
2019-10-11 19:31:41 +0200

    Correction on Mul2

--- Lab1/main.c
+++ Lab1/main.c
@@ -47,14 +47,20 @@ void Mul2(char A[N][N], char B[N][N], int C[N][N])
 {
 	int i, j, k;
 
+  	for (i = 0; i < N; i++)
+    {	
+		for (j = 0; j < N; j++)
+    	{	
+			C[i][j] = 0;
+		}
+	}
+
 	for (i = 0; i < N; i++)
 	{
 		for (j = 0; j < N; j++)
 		{
-			C[j][i] = 0;
-
 			for (k = 0; k < N; k++)
-				C[j][k] += A[j][k] * B[j][k];
+				C[i][k] += A[i][j] * B[j][k];
 		}
 	}
 }
